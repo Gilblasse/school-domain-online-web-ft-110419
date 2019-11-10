0120400463bbdb4a8aa41aa57d11954b7b8e63de
Nethelbert <gilblasse@gmail.com>
2019-11-10 02:03:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,7 @@
 class School
 	attr_accessor :name,:roster,:add_student
+	attr_reader :grade
+	
 
   def initialize(school_name)
 		@name = school_name
@@ -13,7 +15,10 @@ class School
 			@roster[grade] = []
 			@roster[grade] << student
 		end
-
+  end
+  
+  def grade(grade)
+    @roster[grade]
   end
   
 end
