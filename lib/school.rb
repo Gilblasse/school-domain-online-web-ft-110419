34b3aa10767f2dafc5303fe5d8abbd8efaa90e9d
Nethelbert <gilblasse@gmail.com>
2019-11-10 02:01:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,20 @@
 class School
-	attr_reader :name,:roster
-
+	attr_accessor :name,:roster,:add_student
 
   def initialize(school_name)
 		@name = school_name
     @roster = {}
   end
-  
+	
+	def add_student(student,grade)
+		if @roster[grade] 
+			@roster[grade] << student
+		else
+			@roster[grade] = []
+			@roster[grade] << student
+		end
+
+
+  end
   
 end
